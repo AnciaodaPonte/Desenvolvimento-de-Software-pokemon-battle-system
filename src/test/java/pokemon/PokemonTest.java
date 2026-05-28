940b650d12c9d775df0b14ff80bbd65fcdec1a9b
MoisesSac <sacmoise732@gmail.com>
2026-05-28 11:46:41 -0500

    test: agregar casos borde de HP y condicion de derrota

--- src/test/java/pokemon/PokemonTest.java
+++ src/test/java/pokemon/PokemonTest.java
@@ -3,28 +3,60 @@ package pokemon;
 import moves.MoveType;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 class PokemonTest {
 
-    @Test
-    void recibirDanoFisicoDebeReducirElHpCorrectamente() {
-        DefaultPokemon pokemon = new DefaultPokemon(
+    private DefaultPokemon crearPokemonDePrueba(int hp, int defense) {
+        return new DefaultPokemon(
                 1,
                 "Pokemon de prueba",
                 MoveType.NORMAL,
                 null,
                 300,
-                100,
+                hp,
                 50,
-                10,
+                defense,
                 50,
                 10,
                 60
         );
+    }
+
+    @Test
+    void recibirDanoFisicoDebeReducirElHpCorrectamente() {
+        DefaultPokemon pokemon = crearPokemonDePrueba(100, 10);
 
         pokemon.receiveDamage(30);
 
         assertEquals(80, pokemon.getHp());
+    }
+
+    @Test
+    void elHpNuncaDebeQuedarEnValorNegativo() {
+        DefaultPokemon pokemon = crearPokemonDePrueba(100, 10);
+
+        pokemon.receiveDamage(500);
+
+        assertEquals(0, pokemon.getHp());
+    }
+
+    @Test
+    void pokemonConCeroHpDebeQuedarDerrotado() {
+        DefaultPokemon pokemon = crearPokemonDePrueba(100, 10);
+
+        pokemon.receiveDamage(500);
+
+        assertTrue(pokemon.isFainted());
+        assertFalse(pokemon.isAlive());
+    }
+
+    @Test
+    void danoMenorQueLaDefensaNoDebeIncrementarElHp() {
+        DefaultPokemon pokemon = crearPokemonDePrueba(100, 50);
+
+        pokemon.receiveDamage(30);
+
+        assertEquals(100, pokemon.getHp());
     }
 }
